d53949ec3428c16048ebdbcac343c47f709743d7
Kehindebrowns <111283128+Kehindebrowns@users.noreply.github.com>
2025-02-03 10:33:58 +0100

    using variables

--- main.go
+++ main.go
@@ -3,5 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello kehinde")
+	// var smsSendingLimit int
+	// var costPerSMS float64
+	// var hasPermission bool
+	// var username string
+	// fmt.Printf(
+	// 	"%v %f %v %q \n",
+	// 	smsSendingLimit,
+	// 	costPerSMS,
+	// 	hasPermission,
+	// 	username,
+	// )
+	// // var username string = "taiye"
+	// // var password int = 44
+	// fmt.Println("hello kehinde", username)
+	// congrats := "Happy Birthday"
+	// fmt.Println(congrats)
+
+	// penniesPerText := 2.0
+	// fmt.Println("This type of is of", penniesPerText)
+	mileage, company := 222, "10x Developers"
+	fmt.Println(mileage, company)
 }
