369f688cf07e94f684c614ae24e1fcef5aa0c6fa
Kehinde-Crypto <kehindebrowns22@gmail.com>
2025-02-12 10:24:16 +0100

    tutorials on golang

---
+++ tutorial.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt" // standard module to primt things the console
+
+func main() {
+	var tim_name2 string = "Kehinde"
+	tim_name2 = "tola"
+	var number uint8 = 260
+
+	fmt.Println("Hello world", number)
+}
